2ceb3f4b0c01eb5844d160de5482e6b99862222b
fernanda <focoeclareza@gmail.com>
2025-11-15 17:06:56 -0300

    finalizando o "adicionar itens na lista de tarefas".

--- js/controle.js
+++ js/controle.js
@@ -10,14 +10,27 @@ if((valorInput!=="") && (valorInput!==null) && (valorInput!==undefined)){
         <i class="mdi mdi-circle-outline"></i>
       </div>
       <div class="nome">
-        Teste de Tarefa
+        ${valorInput}
       </div>
       <div class="botao">
         <button class="delete"><i class="mdi mdi-delete"></i> Deletar</button>
       </div>
     </div>`;
-
+    
+    //adicionar novo item no main
     main.innerHTML += novoItem;
 
+    //zerar campo de tarefa
+    input.value = "";
+    input.focus();
+
 }
 }
+
+input.addEventListener("keyup",function(event){
+  //se teclou enter (13)
+  if(event.keyCode ===13){
+    event.preventDefault();
+    btnAdd.click();
+  }
+})
