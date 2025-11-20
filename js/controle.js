2afce41673158b5242b0f7e5304effa25d7c5b28
fernanda <focoeclareza@gmail.com>
2025-11-19 21:17:25 -0300

    adicionando icone de itens clicado  nas tarefas.

--- js/controle.js
+++ js/controle.js
@@ -9,10 +9,10 @@ if((valorInput!=="") && (valorInput!==null) && (valorInput!==undefined)){
   
   ++contador;
   let novoItem = `<div id="${contador}" class="itens">
-      <div class="icone">
-        <i class="mdi mdi-circle-outline"></i>
+      <div onclick="marcarTarefa(${contador})" class="itens-icone">
+        <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
       </div>
-      <div class="nome">
+      <div onclick="marcarTarefa(${contador})" class="itens-nome">
         ${valorInput}
       </div>
       <div class="botao">
@@ -33,6 +33,29 @@ if((valorInput!=="") && (valorInput!==null) && (valorInput!==undefined)){
 function deletar(id){
   var tarefa = document.getElementById(id);
   tarefa.remove();
+}
+
+function marcarTarefa(id){
+  var itens = document.getElementById(id);
+  var classe = itens.getAttribute('class');
+  console.log(classe);
+
+  if (classe == "itens") {
+    itens.classList.add('clicado');
+
+    var icone = document.getElementById('icone_'+ id);
+    icone.classList.remove('mdi-circle-outline');
+    icone.classList.add('mdi-check-circle');
+
+    itens.parentNode.appendChild(itens);
+
+  } else{
+    itens.classList.remove('clicado');
+
+    var icone = document.getElementById('icone_'+ id);
+    icone.classList.add('mdi-circle-outline');
+    icone.classList.remove('mdi-check-circle');
+  }
 }
 
 
